0b7bf41d5552f71e276b3ceabb34de2ccbe11946
diker <aldiker@ukr.net>
2023-02-11 23:16:40 +0200

    666

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import ListView, DetailView
+
+from siteblog.settings import BASE_DIR, STATIC_ROOT
 from .models import Post, Category, Tag
 from django.db.models import F
 
@@ -14,6 +16,8 @@ class Home(ListView):
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Classic Blog Design'
+        context['Base_DIR'] = BASE_DIR
+        context['STATIC_ROOT'] = STATIC_ROOT
         return context
 
 
